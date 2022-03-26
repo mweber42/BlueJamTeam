909e73f6e86ad9c7371bdaf23e8fb6ead8521730
Damien <damien.demontis@epitech.eu>
2022-03-26 17:14:54 +0100

    [ADD] working event catcher

--- Blue/background.js
+++ Blue/background.js
@@ -1,6 +1,8 @@
 chrome.runtime.onInstalled.addListener(function() {
     alert("Merci d'avoir installé / mis à jour Blue !");
 });
-chrome.webNavigation.onCompleted.addListener(function() {
-    alert("Tu as chargé google");
-}, {url: [{urlMatches: "google.fr"}]});
+
+
+
+const audio = new Audio( "/music/disfigure.mp3" );
+
---
+++ Blue/js/event1.js
@@ -0,0 +1,10 @@
+var click_count=0;
+
+document.addEventListener("click", function() {
+    click_count +=1;
+    if (click_count == 10) {
+        alert("test");
+        click_count= 0;    
+    } else {
+    }
+}, false);
