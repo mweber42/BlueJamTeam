3f9f9a7938fd390130f7dc6b35925bf1b15a0c77
Damien <damien.demontis@epitech.eu>
2022-03-27 23:33:35 +0200

    [ADD] lol button working

--- Blue/js/button.js
+++ Blue/js/button.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    // your code here
+
+    var click_count=0;
+
+    document.addEventListener("click", function() {
+        click_count +=1;
+        if (click_count == 10) {
+            click_count= 0;    
+        } else {
+        }
+    }, false);
+
+
 var Zorotl = 0;
 
 function initiateClicks() {
@@ -18,15 +32,46 @@ function doClick() {
 }
 
 document.getElementById("clickButton").onclick = doClick;
-initiateClicks();
+
 
 function pay_rancon() {
-    if (Zorotl >= 5) {
-        Zorotl += 2;
+    if (Zorotl >= 1) {
+        Zorotl -= 1;
         
         localStorage.setItem("clicks", Zorotl);
         document.getElementById("clicks").innerHTML = Zorotl;
     }
 }
 
-document.getElementById("rancon_button").onclick = pay_rancon();
+document.getElementById("rancon_button").onclick = pay_rancon;
+
+
+
+function go_quizz() {
+
+    window.open('https://hugokirch.github.io/BLUE_QUIZ/', '_blank');
+
+}
+
+document.getElementById("quizz_button").onclick = go_quizz;
+
+initiateClicks();
+
+
+var departMinutes = 5
+let temps = departMinutes * 60
+
+const timerElement = document.getElementById("timer")
+
+setInterval(() => {
+  let minutes = parseInt(temps / 60, 10)
+  let secondes = parseInt(temps % 60, 10)
+
+  minutes = minutes < 10 ? "0" + minutes : minutes
+  secondes = secondes < 10 ? "0" + secondes : secondes
+
+  timerElement.innerText = `${minutes}:${secondes}`
+  temps = temps <= 0 ? 0 : temps - 1
+}, 1000)
+
+});
--- Blue/js/event1.js
+++ Blue/js/event1.js
@@ -1,9 +1,1 @@
-var click_count=0;
 
-document.addEventListener("click", function() {
-    click_count +=1;
-    if (click_count == 10) {
-        click_count= 0;    
-    } else {
-    }
-}, false);
