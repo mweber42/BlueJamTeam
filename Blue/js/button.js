66a4b809b390cec8271734157e3ea51c7fa35170
Damien <damien.demontis@epitech.eu>
2022-03-28 01:10:07 +0200

    [ADD] working FINAL

--- Blue/js/button.js
+++ Blue/js/button.js
@@ -1,11 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
     // your code here
 
+
+    var Zorotl = 0;
+    
     var click_count=0;
 
     document.addEventListener("click", function() {
+
         click_count += 1;
         if (click_count == 10) {
+            Zorotl += 1;
+            localStorage.setItem("clicks", Zorotl);
+            document.getElementById("clicks").innerHTML = Zorotl;
             window.open("http://zorotl.fr/findme/", '_blank').focus();
             click_count = 0;    
         } else {
@@ -13,7 +20,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }, false);
 
 
-var Zorotl = 0;
+
 
 function initiateClicks() {
     var clickStr = localStorage.getItem("clicks");
@@ -49,7 +56,10 @@ document.getElementById("rancon_button").onclick = pay_rancon;
 
 
 function go_quizz() {
-
+    
+    Zorotl += 3;
+    localStorage.setItem("clicks", Zorotl);
+    document.getElementById("clicks").innerHTML = Zorotl;
     window.open('http://zorotl.fr/quiz/', '_blank');
 
 }
@@ -59,20 +69,5 @@ document.getElementById("quizz_button").onclick = go_quizz;
 initiateClicks();
 
 
-var departMinutes = 5
-let temps = departMinutes * 60
-
-const timerElement = document.getElementById("timer")
-
-setInterval(() => {
-  let minutes = parseInt(temps / 60, 10)
-  let secondes = parseInt(temps % 60, 10)
-
-  minutes = minutes < 10 ? "0" + minutes : minutes
-  secondes = secondes < 10 ? "0" + secondes : secondes
-
-  timerElement.innerText = `${minutes}:${secondes}`
-  temps = temps <= 0 ? 0 : temps - 1
-}, 1000)
 
 });
